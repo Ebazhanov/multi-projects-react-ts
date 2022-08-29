4d645c394f1136c2a8e8e17eae9c4ff9eff7966c
Zhenja <evg.bazhanov@yahoo.com>
2022-08-29 20:53:44 +0200

    add counter design

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.scss";
 
 function App() {
+  let count = 0;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Counter:</h2>
+        <h1>{count}</h1>
+        <button className="minus">- Minus</button>
+        <button className="plus">Plus +</button>
+      </div>
     </div>
   );
 }
