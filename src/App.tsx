eff286fff88ddb209bdd8eec86b1721847440a24
Zhenja <evg.bazhanov@yahoo.com>
2022-08-29 22:24:16 +0200

    add functionality for counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import React from "react";
 import "./index.scss";
+import Counter from "./Counter";
 
 function App() {
-  let count = 0;
-
   return (
-    <div className="App">
-      <div>
-        <h2>Counter:</h2>
-        <h1>{count}</h1>
-        <button className="minus">- Minus</button>
-        <button className="plus">Plus +</button>
-      </div>
+    <div>
+      <Counter />
     </div>
   );
 }
---
+++ src/Counter.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./index.scss";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const onClickPlus = () => setCount(count + 1);
+  const onClickMinus = () => setCount(count - 1);
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Counter:</h2>
+        <h1>{count}</h1>
+        <button className="minus" onClick={onClickMinus}>
+          - Minus
+        </button>
+        <button className="plus" onClick={onClickPlus}>
+          Plus +
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
