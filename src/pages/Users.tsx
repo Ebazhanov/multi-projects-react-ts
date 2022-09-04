34cddf18d8f34ad3820bb2547c1f8337eb4b50f6
ebazhanov <evg.bazhanov@yahoo.com>
2022-09-04 13:44:59 +0200

    was working on User search

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,30 +1,69 @@
-import React from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import { Skeleton } from "../components/users/Skeleton";
 import { User } from "../components/users/User";
 import "./Users.scss";
 
-interface Props {
-  items?: any;
-  isLoading?: any;
-}
-
 export const UsersSearch = () => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => setUsers(json.data))
+      .catch((err) => {
+        console.warn(err);
+        alert("Error while fetching user");
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  //const onChangeSearchValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const onChangeSearchValue = (event: any) => {
+    setSearchValue(event.target.value);
+  };
+
   return (
     <div className="users-main">
-      <Users />
+      <Users
+        onChangeSearchValue={onChangeSearchValue}
+        searchValue={searchValue}
+        items={users}
+        isLoading={isLoading}
+      />
       {/*      <Success />*/}
     </div>
   );
 };
 
-const Users = ({ items, isLoading }: Props) => {
+interface UsersProps {
+  items: any;
+  isLoading: boolean;
+  searchValue: string;
+  onChangeSearchValue: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Users = ({
+  items,
+  isLoading,
+  onChangeSearchValue,
+  searchValue,
+}: UsersProps) => {
+  console.log(searchValue);
+
   return (
     <>
       <div className="search">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input type="text" placeholder="Search for user ..." />
+        <input
+          value={searchValue}
+          onChange={onChangeSearchValue}
+          type="text"
+          placeholder="Search for user ..."
+        />
       </div>
       {isLoading ? (
         <div className="skeleton-list">
@@ -34,7 +73,24 @@ const Users = ({ items, isLoading }: Props) => {
         </div>
       ) : (
         <ul className="users-list">
-          <User />
+          {items
+            .filter((obj: any) => {
+              const fullName = obj.first_name + obj.last_name;
+
+              if (obj.first_name.indexOf(searchValue)) {
+                console.log(obj.first_name);
+                return true;
+              } else {
+                return false;
+              }
+              /*return (
+                fullName.toString().include(searchValue) ||
+                obj.email.toString().include(searchValue)
+              );*/
+            })
+            .map((obj: any) => (
+              <User key={obj.id} {...obj} />
+            ))}
         </ul>
       )}
       <button className="send-invite-btn">Send invitation</button>
